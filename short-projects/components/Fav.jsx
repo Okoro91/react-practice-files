54242a8b759aa01f50f85d8f82832723b47bce29
okoro91 <mi.fracri91@gmail.com>
2025-07-02 16:07:31 +0100

    added new project: react-form

---
+++ short-projects/components/Fav.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "./main.css";
+
+const App = () => {
+  const [myFavoriteThings, setMyFavoriteThings] = React.useState([]);
+
+  const allFavoriteThings = [
+    "💦🌹",
+    "😺",
+    "💡🫖",
+    "🔥🧤",
+    "🟤🎁",
+    "🐴",
+    "🍎🥧",
+    "🚪🔔",
+    "🛷🔔",
+    "🥩🍝",
+  ];
+
+  const thingsElements = myFavoriteThings.map((thing) => (
+    <p key={thing}>{thing}</p>
+  ));
+
+  function addFavoriteThing() {
+    setMyFavoriteThings((prevThings) => [
+      ...prevThings,
+      allFavoriteThings[prevThings.length],
+    ]);
+  }
+
+  return (
+    <main>
+      <button onClick={addFavoriteThing}>Add item</button>
+      <section aria-live="polite">{thingsElements}</section>
+    </main>
+  );
+};
+
+export default App;
--- short-projects/src/App.jsx
+++ short-projects/src/App.jsx
@@ -1,37 +1,79 @@
-import React, { useState } from "react";
+import React from "react";
+import avatar from "../images/user.png";
+import starFilled from "../images/star-filled.png";
+import starEmpty from "../images/star-empty.png";
 import "./App.css";
 
 const App = () => {
-  const [myFavoriteThings, setMyFavoriteThings] = React.useState([]);
+  const [contact, setContact] = React.useState({
+    firstName: "John",
+    lastName: "Doe",
+    phone: "+1 (212) 555-1212",
+    email: "itsmyrealname@example.com",
+    isFavorite: false,
+  });
 
-  const allFavoriteThings = [
-    "💦🌹",
-    "😺",
-    "💡🫖",
-    "🔥🧤",
-    "🟤🎁",
-    "🐴",
-    "🍎🥧",
-    "🚪🔔",
-    "🛷🔔",
-    "🥩🍝",
-  ];
+  let starIcon = contact.isFavorite ? starEmpty : starFilled;
 
-  const thingsElements = myFavoriteThings.map((thing) => (
-    <p key={thing}>{thing}</p>
-  ));
-
-  function addFavoriteThing() {
-    setMyFavoriteThings((prevThings) => [
-      ...prevThings,
-      allFavoriteThings[prevThings.length],
-    ]);
+  function toggleFavorite() {
+    setContact((prev) => ({
+      ...prev,
+      isFavorite: !prev.isFavorite,
+    }));
   }
+
+  const handleForm = (formData) => {
+    const name = formData.get("name");
+    const email = formData.get("email");
+    const phone = formData.get("phone");
+    const password = formData.get("password");
+    console.log(name, email, phone, password);
+  };
 
   return (
     <main>
-      <button onClick={addFavoriteThing}>Add item</button>
-      <section aria-live="polite">{thingsElements}</section>
+      <article className="card">
+        <img
+          src={avatar}
+          className="avatar"
+          alt="User profile picture of John Doe"
+        />
+        <div className="info">
+          <button
+            onClick={toggleFavorite}
+            aria-pressed={contact.isFavorite}
+            aria-label={
+              contact.isFavorite ? "Remove from favorites" : "Add to favorites"
+            }
+            className="favorite-button"
+          >
+            <img
+              src={starIcon}
+              alt={contact.isFavorite ? "filled star icon" : "empty star icon"}
+              className="favorite"
+            />
+          </button>
+          <h2 className="name">
+            {contact.firstName} {contact.lastName}
+          </h2>
+          <p className="contact">{contact.phone}</p>
+          <p className="contact">{contact.email}</p>
+        </div>
+      </article>
+      <article className="form">
+        <h1>Sign Up Form</h1>
+        <form action={handleForm}>
+          <label htmlFor="name">Full Name</label>
+          <input type="text" name="name" id="" />
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="" />
+          <label htmlFor="phone">Pone Number</label>
+          <input type="number" name="phone" id="" />
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="" />
+          <button>Submit</button>
+        </form>
+      </article>
     </main>
   );
 };
